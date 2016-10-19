f4ca644a3e18a61e75a4a12fdffd74ecacf6441d
Sergio Barrera <sdbeng@gmail.com>
2016-10-18 22:56:04 -0700

    Add Box component to Dashboard

---
+++ src/components/Box.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Box extends Component {
+  render(){
+    return (
+      <div>
+      <h2> Box </h2>
+      image
+      <p>comment</p>
+      </div>
+    )
+  }
+}
+
+export default Box
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import Box from './Box'
 
 class Dashboard extends Component {
   render(){
     return (
       <div>
       <h2> Dashboard</h2>
+      <Box />
       </div>
     )
   }
